3bfa5bdd46384c8412e24be6f4f1a9d704fdfbf7
Samuel Sutch <samuraiblog@gmail.com>
2012-11-18 01:14:16 -0800

    basic setup.py file

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name="padlock",
+    version="0.0.1",
+    url='http://github.com/samuraisam/padlock',
+    author='Samuel Sutch',
+    author_email='samuel.sutch@gmail.com',
+    description='A lock library with multiple backends',
+    packages=['padlock'],
+    install_requires=['zope.interface==4.0.1', 'zope.component==4.0.0'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+    ]
+)
